1e42e558b778d999f617a8597579a6163397a6ee
James Bray <yarbsemaj@gmail.com>
2025-10-25 00:12:09 +0100

    chore: lint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,45 @@
 <script lang="ts">
-	import ThreeD from '../components/ThreeD.svelte';
-	import type { TypedItem } from '../components/typewriter/types';
-	import Typewriter from '../components/typewriter/Typewriter.svelte';
+	import ThreeD from "../components/ThreeD.svelte";
+	import type { TypedItem } from "../components/typewriter/types";
+	import Typewriter from "../components/typewriter/Typewriter.svelte";
 
-	import Fa from 'svelte-fa';
-	import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
-	import { faGithub, faLinkedin, faMastodon } from '@fortawesome/free-brands-svg-icons';
-	import { onMount } from 'svelte';
-	import { check_webp_feature } from '$lib/utils';
-	import Keyboard from '../components/keyboard/Keyboard.svelte';
+	import Fa from "svelte-fa";
+	import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+	import {
+		faGithub,
+		faLinkedin,
+		faMastodon,
+	} from "@fortawesome/free-brands-svg-icons";
+	import { onMount } from "svelte";
+	import { check_webp_feature } from "$lib/utils";
+	import Keyboard from "../components/keyboard/Keyboard.svelte";
 
 	let inputKey: (key: string) => void;
 
 	let showKeyboard = false;
 
 	let typedLines: Array<TypedItem> = [
-		{ text: 'React' },
-		{ text: 'PHP' },
-		{ text: 'SQL' },
-		{ text: 'Svelte' },
-		{ text: 'Z80 Assembly', url: 'https://z80.yarbsemaj.com' },
-		{ text: 'NodeJS' },
-		{ text: 'Serverless' },
-		{ text: 'Dynamodb' },
-		{ text: 'Lambda' },
-		{ text: 'CDK' },
-		{ text: 'AngularJS' },
-		{ text: 'Tiny CPU ASM', url: 'https://tiny-cpu-emu.yarbsemaj.com' },
-		{ text: 'Javascript' },
-		{ text: 'Java' },
-		{ text: 'Tailwindcss' }
+		{ text: "React" },
+		{ text: "PHP" },
+		{ text: "SQL" },
+		{ text: "Svelte" },
+		{ text: "Z80 Assembly", url: "https://z80.yarbsemaj.com" },
+		{ text: "NodeJS" },
+		{ text: "Serverless" },
+		{ text: "Dynamodb" },
+		{ text: "Lambda" },
+		{ text: "CDK" },
+		{ text: "AngularJS" },
+		{ text: "Tiny CPU ASM", url: "https://tiny-cpu-emu.yarbsemaj.com" },
+		{ text: "Javascript" },
+		{ text: "Java" },
+		{ text: "Tailwindcss" },
 	];
 
 	onMount(() => {
-		check_webp_feature('lossy', (feature, supported) => {
+		check_webp_feature("lossy", (feature, supported) => {
 			if (!supported) {
-				window.location.href = '/old-browser';
+				window.location.href = "/old-browser";
 			}
 		});
 	});
@@ -43,7 +47,10 @@
 
 <svelte:head>
 	<title>James Bray</title>
-	<meta name="description" content="Full Stack Software Developer working in Manchester" />
+	<meta
+		name="description"
+		content="Full Stack Software Developer working in Manchester"
+	/>
 	<meta name="og:image" content="https://yarbsemaj.com/img/og-image.jpeg" />
 	<link rel="preload" as="image" href="/img/loader.webp" />
 	<link
@@ -94,11 +101,18 @@
 			I'm James, a Full Stack Software Engineer based in Manchester.
 		</div>
 		<div class="my-6 md:my-10">
-			<Typewriter className="sm:text-6xl text-3xl font-bold font-mono" {typedLines} />
+			<Typewriter
+				className="sm:text-6xl text-3xl font-bold font-mono"
+				{typedLines}
+			/>
 		</div>
 
 		<div class="flex justify-center sm:gap-10 gap-2">
-			<a aria-label="Github" class="sm:text-8xl text-6xl p-4" href="https://github.com/yarbsemaj">
+			<a
+				aria-label="Github"
+				class="sm:text-8xl text-6xl p-4"
+				href="https://github.com/yarbsemaj"
+			>
 				<Fa icon={faGithub} />
 			</a>
 			<a
@@ -108,7 +122,11 @@
 			>
 				<Fa icon={faLinkedin} />
 			</a>
-			<a class="sm:text-8xl text-6xl p-4" aria-label="Blog" href="https://blog.yarbsemaj.com">
+			<a
+				class="sm:text-8xl text-6xl p-4"
+				aria-label="Blog"
+				href="https://blog.yarbsemaj.com"
+			>
 				<Fa icon={faLaptopCode} />
 			</a>
 			<a
@@ -122,13 +140,14 @@
 		</div>
 	</div>
 	<div class="text-center text-sm pb-2 absolute bottom-0 text-gray-400">
-		<a class="underline" href="https://skfb.ly/ou69O">Retro computer model by Urpo, </a> modify by James
-		Bray
+		<a class="underline" href="https://skfb.ly/ou69O"
+			>Retro computer model by Urpo,
+		</a> modify by James Bray
 	</div>
 </div>
 
 <style>
-	@import url('https://fonts.googleapis.com/css?family=Courier+Prime&display=swap');
+	@import url("https://fonts.googleapis.com/css?family=Courier+Prime&display=swap");
 	:global(body) {
 		background-color: black;
 		color: white;
--- src/routes/old-browser/+page.svelte
+++ src/routes/old-browser/+page.svelte
@@ -1,50 +1,59 @@
 <script lang="ts">
-	import type { TypedItem } from '../../components/typewriter/types';
-	import Typewriter from '../../components/typewriter/Typewriter.svelte';
-	import background from './old-browser.jpeg'
+	import type { TypedItem } from "../../components/typewriter/types";
+	import Typewriter from "../../components/typewriter/Typewriter.svelte";
+	import background from "./old-browser.jpeg";
 
-	import Fa from 'svelte-fa';
-	import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
-	import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-	import { onMount } from 'svelte';
-	import { check_webp_feature } from '$lib/utils';
+	import Fa from "svelte-fa";
+	import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+	import {
+		faGithub,
+		faLinkedin,
+		faMastodon,
+	} from "@fortawesome/free-brands-svg-icons";
+	import { onMount } from "svelte";
+	import { check_webp_feature } from "$lib/utils";
 
 	let typedLines: Array<TypedItem> = [
-		{ text: 'React' },
-		{ text: 'PHP' },
-		{ text: 'SQL' },
-		{ text: 'Svelte' },
-		{ text: 'Z80 Assembly', url: 'https://z80.yarbsemaj.com' },
-		{ text: 'NodeJS' },
-		{ text: 'Serverless' },
-		{ text: 'Dynamodb' },
-		{ text: 'Lambda' },
-		{ text: 'CDK' },
-		{ text: 'AngularJS' },
-		{ text: 'Tiny CPU ASM', url: 'https://tiny-cpu-emu.yarbsemaj.com' },
-		{ text: 'Javascript' },
-		{ text: 'Java' },
-		{ text: 'Tailwindcss' }
+		{ text: "React" },
+		{ text: "PHP" },
+		{ text: "SQL" },
+		{ text: "Svelte" },
+		{ text: "Z80 Assembly", url: "https://z80.yarbsemaj.com" },
+		{ text: "NodeJS" },
+		{ text: "Serverless" },
+		{ text: "Dynamodb" },
+		{ text: "Lambda" },
+		{ text: "CDK" },
+		{ text: "AngularJS" },
+		{ text: "Tiny CPU ASM", url: "https://tiny-cpu-emu.yarbsemaj.com" },
+		{ text: "Javascript" },
+		{ text: "Java" },
+		{ text: "Tailwindcss" },
 	];
 
-	onMount(()=>{
-		check_webp_feature('lossy', (feature, supported)=>{
-			if(supported){
-				window.location.href = '/'
+	onMount(() => {
+		check_webp_feature("lossy", (feature, supported) => {
+			if (supported) {
+				window.location.href = "/";
 			}
-		})
-	})
+		});
+	});
 </script>
 
 <svelte:head>
 	<title>James Bray</title>
-	<meta name="description" content="Full Stack Software Developer working in Manchester" />
+	<meta
+		name="description"
+		content="Full Stack Software Developer working in Manchester"
+	/>
 	<meta name="og:image" content="https://yarbsemaj.com/img/og-image.jpeg" />
-	<meta name="robots" content="noindex">
+	<meta name="robots" content="noindex" />
 </svelte:head>
 
-<div class="h-screen w-screen flex flex-col justify-center items-center z-10 absolute blur-sm opacity-50">
-	<img alt='background' src={background} />
+<div
+	class="h-screen w-screen flex flex-col justify-center items-center z-10 absolute blur-sm opacity-50"
+>
+	<img alt="background" src={background} />
 </div>
 
 <div class="h-screen flex flex-col justify-center items-center z-10 relative">
@@ -58,11 +67,18 @@
 			I'm James, a Full Stack Software Engineer based in Manchester.
 		</div>
 		<div class="my-6 md:my-10">
-			<Typewriter className="sm:text-6xl text-3xl font-bold font-mono" {typedLines} />
+			<Typewriter
+				className="sm:text-6xl text-3xl font-bold font-mono"
+				{typedLines}
+			/>
 		</div>
 
 		<div class="flex justify-center sm:gap-10 gap-2">
-			<a aria-label="Github" class="sm:text-8xl text-6xl p-4" href="https://github.com/yarbsemaj">
+			<a
+				aria-label="Github"
+				class="sm:text-8xl text-6xl p-4"
+				href="https://github.com/yarbsemaj"
+			>
 				<Fa icon={faGithub} />
 			</a>
 			<a
@@ -75,9 +91,17 @@
 			<a
 				class="sm:text-8xl text-6xl p-4"
 				aria-label="Blog"
-				href="/blog"
+				href="https://blog.yarbsemaj.com"
 			>
 				<Fa icon={faLaptopCode} />
+			</a>
+			<a
+				class="sm:text-8xl text-6xl p-4"
+				rel="me"
+				aria-label="Mastodon"
+				href="https://me.bray.im/@james"
+			>
+				<Fa icon={faMastodon} />
 			</a>
 		</div>
 	</div>
@@ -87,7 +111,7 @@
 </div>
 
 <style>
-	@import url('https://fonts.googleapis.com/css?family=Courier+Prime&display=swap');
+	@import url("https://fonts.googleapis.com/css?family=Courier+Prime&display=swap");
 	:global(body) {
 		background-color: black;
 		color: white;
